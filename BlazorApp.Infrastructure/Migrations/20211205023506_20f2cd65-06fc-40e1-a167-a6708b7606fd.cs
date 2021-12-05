370150c6df221bf7bd5a1135d3c07f08188c62f1
Alaa0129 <alia@itu.dk>
2021-12-05 03:37:27 +0100

    Merge done

---
+++ BlazorApp.Infrastructure/Migrations/20211205023506_20f2cd65-06fc-40e1-a167-a6708b7606fd.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorApp.Infrastructure.Migrations
+{
+    public partial class _20f2cd6506fc40e1a167a6708b7606fd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
